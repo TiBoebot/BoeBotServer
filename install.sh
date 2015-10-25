ddc9782c2f28b95fde527634633d35fd0c721446
Johan Talboom <borfje@gmail.com>
2015-10-25 17:02:49 +0000

    Made install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+
+apt-get install -y oracle-java8-jdk git
+
+
+#install pigpio
+cd ~
+wget abyz.co.uk/rpi/pigpio/pigpio.zip
+unzip pigpio.zip
+cd PIGPIO
+make
+make install
+
+
+#install server
+cd $(dirname $0)
+BASEDIR=$(pwd -P)
+javac server/*.java
+ln -s /usr/bin/java /usr/bin/serverjava
+
+if grep -Fxq "serverjava" /etc/rc.local 
+then
+echo Already installed in rc.local
+else
+sed -i "s#exit 0#cd ${BASEDIR}\nserverjava server/Main\nexit 0#g" /etc/rc.local
+fi
+
+#install boebot library
+cd /home/pi
+git clone https://github.com/TiBoebot/BotBotLib.git /home/pi/BoeBotLib
+mkdir /home/pi/upload
+chmod 777 /home/pi/upload
+chown pi:pi /home/pi/upload
