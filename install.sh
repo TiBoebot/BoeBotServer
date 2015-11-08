2c4b63d32bb5668d79fa98980c437fe16f0648b8
Johan Talboom <borfje@gmail.com>
2015-11-08 19:13:04 +0000

    Fixed install script a bit

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-
+cd $(dirname $0)
+BASEDIR=$(pwd -P)
 apt-get install -y oracle-java8-jdk git
 
 
 #install pigpio
-cd ~
+cd /home/pi/
 wget abyz.co.uk/rpi/pigpio/pigpio.zip
 unzip pigpio.zip
 cd PIGPIO
@@ -12,22 +13,24 @@ make install
 
 
 #install server
-cd $(dirname $0)
-BASEDIR=$(pwd -P)
+cd $(BASEDIR)
 javac server/*.java
 ln -s /usr/bin/java /usr/bin/serverjava
 
-if grep -Fxq "serverjava" /etc/rc.local 
+if grep -Fq "serverjava" /etc/rc.local 
 then
 echo Already installed in rc.local
 else
-sed -i "s#exit 0#cd ${BASEDIR}\nserverjava server/Main &\nexit 0#g" /etc/rc.local
+sed -i "s#^exit 0#cd ${BASEDIR}\nserverjava server/Main &\n\nexit 0#g" /etc/rc.local
 fi
 
 #install boebot library
 cd /home/pi
 git clone https://github.com/TiBoebot/BotBotLib.git /home/pi/BoeBotLib
-cp /home/PIGPIO/libpgpio.so /home/pi/BoeBotLib
+cd /home/pi/BoeBotLib
+/home/pi/BoeBotLib/build
+
+cp /home/pi/PIGPIO/libpigpio.so /home/pi/BoeBotLib
 mkdir /home/pi/upload
 chmod 777 /home/pi/upload
 chown pi:pi /home/pi/upload
